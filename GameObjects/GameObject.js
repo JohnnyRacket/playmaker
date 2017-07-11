3f47653ad885892cfacb27f881c37fd748063df5
John Harrison <johnharrison@johns-mbp.attlocal.net>
2017-07-10 23:29:53 -0500

    finally some legitmacy to the setup, building to js folder and ignoring it, using yarn

--- GameObjects/GameObject.js
+++
@@ -1,9 +0,0 @@
-var GameObject = (function () {
-    function GameObject() {
-    }
-    GameObject.prototype.update = function () {
-        //do something
-    };
-    return GameObject;
-}());
-//# sourceMappingURL=GameObject.js.map
