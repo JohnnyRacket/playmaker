db4ca8d4db8ed8b4dbcfe0ac9cdd25fa0f44cc17
John Harrison <johnharrison@johns-mbp.attlocal.net>
2017-07-10 23:36:25 -0500

    fixes

--- app/GameEngine.ts
+++ app/GameEngine.ts
@@ -34,21 +34,21 @@ export class GameEngine{
      * and is used to progress the game through it's sequence
      */
     public tick(){
-        this.observers.forEach((obj: GameObject, index) => obj.update());
+        this.observers.forEach((obj: IGameObject, index) => obj.update());
     }
 
     /*
      * Register adds game objects to the list of observers
      * to be updated throught the game as time passes
      */
-    public register(obj: GameObject){
+    public register(obj: IGameObject){
         this.observers.push(obj);
     }
     /*
      * Unregister removes game objects from being updated as time
      * progresses in the game
      */
-    public unregister(obj: GameObject){
+    public unregister(obj: IGameObject){
         array.pull(this.observers, obj);
     }
 }
