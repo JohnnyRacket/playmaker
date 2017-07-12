e7cffcda761460ec26feaff70b4ff938691c1990
John Harrison <johnharrison@johns-mbp.attlocal.net>
2017-07-12 17:38:21 -0500

    come controller stuff

---
+++ src/Controllers/RouteController.ts
@@ -0,0 +1,7 @@
+import { Route } from './Route';
+
+export class RouteController{
+    //I expect this class to change as we think about what needs to happen
+    protected route: Route;
+    //more to come, need ot think through collisions first
+}
