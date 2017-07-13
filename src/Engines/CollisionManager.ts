e18d382d0827a9d1f4a2d8447911894bfc5a56ee
John Harrison <johnharrison@johns-mbp.attlocal.net>
2017-07-12 22:51:34 -0500

    hitboxuru

--- src/Engines/CollisionManager.ts
+++ src/Engines/CollisionManager.ts
@@ -1,15 +1,17 @@
+import { ActiveHitbox } from '../Collisions/ActiveHitbox';
+import { ColliableHitbox } from '../Collisions/CollidableHitbox';
 import { CollidableGameObject } from '../GameObjects/CollidableGameObject';
 import { IGameObject } from '../GameObjects/GameObject.interface';
 
 export class CollisionManager implements IGameObject{
 
 
-    private collidables: CollidableGameObject[] = [];
-    private psuedoCollidables: CollidableGameObject[] = [];
+    private collidables: ActiveHitbox[] = [];
+    private psuedoCollidables: ColliableHitbox[] = [];
 
     tick(): void {
         for(let i = 0; i < this.collidables.length; ++i){
-            if()
+            
         }
     }
 }
