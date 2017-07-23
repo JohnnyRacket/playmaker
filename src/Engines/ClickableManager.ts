4c31b065971fa09672c5f44729b3a0e1821d9f50
John Harrison <john.randolph.harrison@gmail.com>
2017-07-23 15:59:06 -0500

    some progress

--- src/Engines/ClickableManager.ts
+++ src/Engines/ClickableManager.ts
@@ -1,3 +1,23 @@
+import { Clickable } from '../Clickables/Clickable';
 export class ClickableManager{
-    
+
+    private clickables: Clickable[];
+
+    public constructor(canvas : HTMLCanvasElement){
+        canvas.addEventListener('click', (evt) => {
+            this.clickEvents(evt);
+        },false);
+
+        // canvas.addEventListener('mouseover', (evt) => {
+        //     this.clickEvents(evt);
+        // },false);
+
+        // canvas.addEventListener('click', (evt) => {
+        //     this.clickEvents(evt);
+        // },false);
+    }
+
+    private clickEvents(event: MouseEvent){
+        this.clickables.forEach((obj: Clickable, index) => obj.click());
+    }
 }
