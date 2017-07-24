15e609fd68419bfdb172eefd4c704064f14d3fd9
John Harrison <john.randolph.harrison@gmail.com>
2017-07-23 21:31:51 -0500

    some view stuff built up

---
+++ src/ViewComposition/ComposableView.ts
@@ -0,0 +1,32 @@
+import { Dimensionable } from '../Shared/Dimensionable';
+import { IViewObject } from '../ViewObjects/ViewObject.interface';
+export class ComposableView extends Dimensionable implements IViewObject{
+
+    //the idea here is to have composition pattern of relative layouts 
+    protected canvas: HTMLCanvasElement;
+    protected context: CanvasRenderingContext2D;
+    private children: IViewObject[] = [];
+
+    public constructor(x: number, y: number, width: number, height: number){
+        super();
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.canvas = new HTMLCanvasElement();
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.context = this.canvas.getContext('2d');
+    }
+
+    render(context: CanvasRenderingContext2D, width: number, height: number) {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.children.forEach((obj: IViewObject, index) => obj.render(this.context, this.canvas.width, this.canvas.height));
+        context.drawImage(this.canvas, this.x, this.y);
+    }
+
+    update() {
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+    }
+}
---
+++ src/ViewComposition/Decorators/VerticalCenterDecorator.ts
@@ -0,0 +1,28 @@
+import { ComposableView } from '../ComposableView';
+export class HorizontalCenterDecorator extends ComposableView{
+
+    protected view: ComposableView;
+
+    get x(): number{
+        return this.view.x;
+    }
+    get y(): number{
+        return this.view.y;
+    }
+    get width(): number{
+        return this.view.width;
+    }
+    get height(): number{
+        return this.view.height;
+    }
+
+    public constructor(view: ComposableView){
+        super(view.x, view.y, view.width, view.height);
+    }
+
+    public render(context: CanvasRenderingContext2D, width: number, height: number){
+        //can set x/y here
+        this.view.y = height/2 - this.height/2;
+        this.view.render(context, width, height);
+    }
+}
